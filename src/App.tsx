dfcf4d3a60a9848c956d9c4703e31831a75dc5b8
gabriellazaroni <gabriellazaroni@outlook.com.br>
2025-05-05 17:48:53 -0300

    create a new Info card, Pricing section, pricing-plans data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Layout } from "./components/Layout"
+import { AboutUs } from "./components/sections/AboutUs"
 import { Brands } from "./components/sections/Brands"
 import { Hero } from "./components/sections/Hero"
+import { Pricing } from "./components/sections/Pricing"
 import { Services } from "./components/sections/Services"
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Hero />
       <Brands />
       <Services />
+      <AboutUs />
+      <Pricing />
     </Layout>
   )
 }
